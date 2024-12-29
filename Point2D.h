dd845812fcecfdc23594bbbaa8388f326c71535b
stinki <taryous-03@hotmail.fr>
2024-12-29 10:31:31 +0100

    Añadida declaración de la clase Point2D

---
+++ Point2D.h
@@ -0,0 +1,20 @@
+#ifndef POINT2D_H
+#define POINT2D_H
+
+#include <iostream>
+#include <cmath>
+
+class Point2D {
+public:
+    double x;
+    double y;
+
+    Point2D(double x = 0, double y = 0);
+    static double distance(const Point2D &a, const Point2D &b);
+    bool operator==(const Point2D &a) const;
+    bool operator!=(const Point2D &a) const;
+    friend std::ostream& operator<<(std::ostream &out, const Point2D &p);
+};
+
+#endif
+
